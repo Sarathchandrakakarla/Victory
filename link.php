4cd9993628d03bf9132ad0802dee1357ee12432b
Sarathchandra Reddy Kakarla <kakarlasarath4@gmail.com>
2024-06-07 12:48:15 +0530

    test update

--- link.php
+++ link.php
@@ -4,4 +4,4 @@ if ($link === false) {
     echo "<script>alert('Could not Connect to Database!')
     location.replace('index.php')</script>";
     //die("ERROR: Could not connect. " . mysqli_connect_error());
-}
+} 
